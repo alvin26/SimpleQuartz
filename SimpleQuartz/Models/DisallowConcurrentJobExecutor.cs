b19c3594d9218bde6c49638b277d42326dcf6505
Alvin.Chen <alvin.chen@collaborate.tw>
2023-05-31 01:04:53 +0800

    add DisallowConcurrentJobExecutor.cs

---
+++ SimpleQuartz/Models/DisallowConcurrentJobExecutor.cs
@@ -0,0 +1,14 @@
+﻿using Quartz;
+
+namespace SimpleQuartz.Models
+{
+    [DisallowConcurrentExecution]
+    public class DisallowConcurrentJobExecutor : IJob
+
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
